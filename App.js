cec16b9e26b3c0a67226cda8e63ff8e2c5038727
m4nudev <manudev912@gmail.com>
2025-03-18 22:52:44 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 20 }}>Hello React Native</Text>
+      <Text style={{ fontSize: 22 }}>Let's program in React Native</Text>
+      <Text style={{ fontSize: 25 }}>It's cool to program in React Native</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#bbb", // GC leong 11+12 geography 11+12 politics
+  },
+  textContainer: {
+    fontSize: 20,
   },
 });
